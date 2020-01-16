1532b3e6a77c829d988fdb56b1cc825f72601db9
wq <>
2020-01-16 22:27:11 +0800

    no message

---
+++ WXMCategory/UIResponder+WXMCategory.h
@@ -0,0 +1,22 @@
+//
+//  UIResponder+WXMCategory.h
+//  Multi-project-coordination
+//
+//  Created by wq on 2020/1/16.
+//  Copyright © 2020 wxm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIResponder (WXMCategory)
+
+/** 响应链传递给父类 */
+- (void)routerEvent:(NSString *)event;
+
+- (void)routerEvent:(NSString *)event object:(nullable id)object;
+
+@end
+
+NS_ASSUME_NONNULL_END
